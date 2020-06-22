0b4f3fad04f62cd85f0946a25e718f008558b8b7
lengfangbing <lengfangbing@bytedance.com>
2020-06-22 14:55:38 +0800

    test: check questions

--- application.ts
+++ application.ts
@@ -202,6 +202,7 @@ export class Application {
       try{
         config = (await import (join(cwd, './min.config.ts'))).default;
       }catch(e){
+        console.log(e);
         throw Error ('no such file named min.config.ts, please check the name or provide a min.config.ts by yourself ');
       }
     }else{
