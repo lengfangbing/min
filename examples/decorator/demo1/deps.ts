c7e55356aadf425fba24b3fcbfe7986c56a55bef
lengfangbing <lengfangbing@bytedance.com>
2020-10-28 14:30:50 +0800

    feat: update deps

--- examples/decorator/demo1/deps.ts
+++ examples/decorator/demo1/deps.ts
@@ -11,5 +11,6 @@ export {
   Route,
   Start,
   StartApplication,
-} from "https://raw.githubusercontent.com/lengfangbing/min/master/decorator/mod.ts";
+// } from "https://raw.githubusercontent.com/lengfangbing/min/master/decorator/mod.ts";
 // the same to https://raw.githubusercontent.com/lengfangbing/min/master/mod.ts.
+} from '../../../decorator/mod.ts';
