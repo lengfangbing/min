014e4b8d5d12bbf57acabb4b780195293e4bfadd
lengfangbing <lengfangbing@gmail.com>
2021-05-05 12:10:50 +0800

    feat: 取消路由树的key中的/开头

--- v2/constants.ts
+++ v2/constants.ts
@@ -1,4 +1,4 @@
 // 动态路由的key
-export const DYNAMIC_ROUTER_TREE_KEY = "/#";
+export const DYNAMIC_ROUTER_TREE_KEY = "#";
 // 全局路由的key
-export const GLOBAL_ROUTER_TREE_KEY = "/##";
+export const GLOBAL_ROUTER_TREE_KEY = "##";
