94a9062a9fb6549aa1cc9559bca9151b99fb6c86
lengfangbing <lengfangbing@gmail.com>
2021-05-05 10:54:16 +0800

    feat: 删除第三方依赖, 最后无法自己实现时再去通过第三方依赖实现

--- v2/deps.ts
+++ v2/deps.ts
@@ -44,13 +44,3 @@ export { assertEquals }  from 'https://deno.land/std@0.95.0/testing/asserts.ts';
 export type {
   FormFile,
 } from "https://deno.land/std@0.95.0/mime/multipart.ts";
-
-/**
- * third dependences export
- */
- export {
-  charset,
-  contentType,
-  lookup,
-} from "https://deno.land/x/media_types@v2.5.1/mod.ts";
-export { is } from "https://deno.land/x/type_is@1.0.0/mod.ts";
