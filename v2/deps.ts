c4aebf6248de6b904f1d514410c5eac3fae168cb
lengfangbing <lengfangbing@gmail.com>
2021-05-22 00:46:32 +0800

    feat: 修改依赖的版本号

--- v2/deps.ts
+++ v2/deps.ts
@@ -6,18 +6,18 @@
   Server,
   ServerRequest,
   serveTLS,
-} from "https://deno.land/std@0.95.0/http/server.ts";
-export { getCookies, deleteCookie, setCookie } from "https://deno.land/std@0.95.0/http/cookie.ts";
-export { decode, encode } from "https://deno.land/std@0.95.0/encoding/base64.ts";
+} from "https://deno.land/std@0.97.0/http/server.ts";
+export { getCookies, deleteCookie, setCookie } from "https://deno.land/std@0.97.0/http/cookie.ts";
+export { decode, encode } from "https://deno.land/std@0.97.0/encoding/base64.ts";
 export {
   parse,
   stringify,
   escape,
   unescape,
-} from "https://deno.land/std@0.95.0/node/querystring.ts";
+} from "https://deno.land/std@0.97.0/node/querystring.ts";
 export {
   MultipartReader,
-} from "https://deno.land/std@0.95.0/mime/multipart.ts";
+} from "https://deno.land/std@0.97.0/mime/multipart.ts";
 export {
   basename,
   extname,
@@ -26,13 +26,14 @@ export {
   normalize,
   resolve,
   sep,
-} from "https://deno.land/std@0.95.0/path/mod.ts";
+} from "https://deno.land/std@0.97.0/path/mod.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/std@0.95.0/http/http_status.ts";
-export * as colors from "https://deno.land/std@0.95.0/fmt/colors.ts";
-export { assertEquals }  from 'https://deno.land/std@0.95.0/testing/asserts.ts';
+} from "https://deno.land/std@0.97.0/http/http_status.ts";
+export * as colors from "https://deno.land/std@0.97.0/fmt/colors.ts";
+export { assertEquals }  from 'https://deno.land/std@0.97.0/testing/asserts.ts';
+export { readAll } from 'https://deno.land/std@0.97.0/io/util.ts'
 
 /**
  * std type export
@@ -40,10 +41,10 @@ export { assertEquals }  from 'https://deno.land/std@0.95.0/testing/asserts.ts';
  export type {
   HTTPSOptions,
   Response,
-} from "https://deno.land/std@0.95.0/http/server.ts";
+} from "https://deno.land/std@0.97.0/http/server.ts";
 export type {
   Cookie
-} from "https://deno.land/std@0.95.0/http/cookie.ts";
+} from "https://deno.land/std@0.97.0/http/cookie.ts";
 export type {
   FormFile,
-} from "https://deno.land/std@0.95.0/mime/multipart.ts";
+} from "https://deno.land/std@0.97.0/mime/multipart.ts";
