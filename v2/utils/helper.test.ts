9d2e6b4a725de9db281667d0dfda62768d0a1e58
lengfangbing <lengfangbing@gmail.com>
2021-05-26 23:33:29 +0800

    feat: 增加提示信息

--- v2/utils/helper.test.ts
+++ v2/utils/helper.test.ts
@@ -1,4 +1,7 @@
-// 构造header
+/**
+ * 仅用于单测时的辅助函数
+ * @param {Record<string, string>} [value] 要映射成Header的k-v
+ */
 export function getHeaders(value?: Record<string, string>) {
   const headers = new Headers();
   Object.keys(value || {}).forEach((item) => {
