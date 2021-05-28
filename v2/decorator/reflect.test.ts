894df874035db00ac2733089fa4248118955cec2
lengfangbing <lengfangbing@gmail.com>
2021-05-28 11:00:37 +0800

    feat: 修改反射获取值的方法

--- v2/decorator/reflect.test.ts
+++ v2/decorator/reflect.test.ts
@@ -12,8 +12,8 @@ export type ReflectRuleHandlerLike = (
  * @param {Array<Array<string>>} [properties]
  * @default 当内部出现错误就会返回void 0
  * @example
- * getValueWithCtx(ctx, [['request', 'query']]), 这个就是获取query的方法
- * getValueWithCtx(ctx, [['request', 'query', 'id'], ['request', 'body']]), 这个就是获取query中的id的方法
+ * getValueWithCtx(reflectValue, ['request', 'query']), 这个就是获取query的方法
+ * getValueWithCtx(reflectValue, ['request', 'query', 'id']), 这个就是获取query中的id的方法
  */
 export function getValueByReflect(reflectValue: Record<string, unknown>, properties: Array<string>) {
   try {
@@ -49,25 +49,3 @@ Deno.test({
     assertEquals(getValueByReflect(ctx, ['request', 'body', 'value']), { name: 'lfb', age: 24 });
   },
 });
-
-//
-// // 每一个规则都有特定的处理方法，方法只能通过ctx来处理返回真正的值
-//
-// /**
-//  *
-//  * 反射的规则
-//  */
-// export const REFLECT_RULES = {
-// } as Record<string, ReflectRuleHandlerLike>;
-//
-// /**
-//  * 添加一组规则的方法
-//  * @param {string} key - 规则的key唯一标识，只用来去重
-//  * @param {ReflectRuleHandlerLike} handler - 该规则的key的解析方法
-//  */
-// export function addOneRule(key: string, handler: ReflectRuleHandlerLike) {
-//   REFLECT_RULES[key] = handler;
-// }
-
-// 内部的自带的规则
-// addOneRule('query', getQueryValueWithCtx);
